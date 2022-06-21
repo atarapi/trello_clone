334237507d9902a2a8e393b854a188141ce79016
Atarapi <40025706+atarapi@users.noreply.github.com>
2022-06-22 02:23:16 +0900

    タスクカード削除の作成

--- src/components/task/TaskCards.jsx
+++ src/components/task/TaskCards.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { AddTaskCardButton } from './button/AddTaskCardButton'
-import { TaskCard } from './TaskCard'
+import React, {useState} from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+import { AddTaskCardButton } from './button/AddTaskCardButton';
+import { TaskCard } from './TaskCard';
 
 export default function TaskCards() {
+  const [taskCardsList, setTaskCardsList] = useState([{
+    id: 0,
+    draggableId: 0,
+  }]);
   return (
-    <div>
-        <TaskCard/>
-        <AddTaskCardButton/>
+    <div className='taskCardsArea'>
+       {taskCardsList.map((taskCardList) => (
+        <TaskCard key={taskCardList.id}/>
+       ))}
+        <AddTaskCardButton 
+        taskCardsList={taskCardsList}
+        setTaskCardsList={setTaskCardsList}/>
     </div>
   )
 }
--- src/components/task/button/TaskCardDeleteButton.jsx
+++ src/components/task/button/TaskCardDeleteButton.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 export const TaskCardDeleteButton = () => {
+  const  taskCardDeleteButton = () =>{
+    
+  }
   return (
-    <div></div>
+    <div>
+      <button className='taskCardDeleteButton' onClick={() => taskCardDeleteButton()}><i className="fa-solid fa-xmark"></i></button>
+    </div>
   )
 }
